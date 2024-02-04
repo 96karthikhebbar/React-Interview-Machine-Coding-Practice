432a0a149dc1d78587b3576b64f759f599f8d562
Karthik  Hebbar <96karthikhebbar@gmail.com>
2024-02-04 23:04:10 +0530

    Completed Phone Number Input,phNum validation & conditional redering of phNum or Otp Input Field

--- src/components/PhoneOtpForm.jsx
+++ src/components/PhoneOtpForm.jsx
@@ -1,17 +1,50 @@
-import {useState} from 'react'
+import { useState } from 'react';
 
 const PhoneOtpForm = () => {
-  return (
-    <div>
-        <form onSubmit={() => {}}>
-            <input 
-                type="text" 
-                value={phoneNumber}
-                onChange={handlePhoneNumChange}
-            />
-        </form>
-    </div>
-  )
-}
+	const [phoneNumber, setPhoneNumber] = useState('');
+	const [showOtpInput, setShowOtpInput] = useState(false);
 
-export default PhoneOtpForm
+	const handlePhoneNumChange = (event) => {
+		setPhoneNumber(event.target.value);
+	};
+
+	function handlePhoneNumSubmit(event) {
+		event.preventDefault();
+
+		// Phone Num Validattion
+		const regex = /[^0-9]/g;
+		if (phoneNumber.length < 10 || regex.test(phoneNumber)) {
+			alert('Invalid Phone Number');
+			return;
+		}
+
+		// call Back-End API
+
+		// Show input Field
+		setShowOtpInput(true);
+
+		return alert('Hi.... Test123!!!');
+	}
+
+	return (
+		<div>
+			{!showOtpInput ? (
+				<form onSubmit={handlePhoneNumSubmit}>
+					<input
+						type='text'
+						value={phoneNumber}
+						onChange={handlePhoneNumChange}
+						placeholder='Enter Phone Number'
+					/>
+					<button type='submit'>Submmit</button>
+				</form>
+			) : (
+				<div>
+					<p>Enter Otp sent to {phoneNumber}</p>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default PhoneOtpForm;
